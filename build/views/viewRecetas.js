c8536528498ae695c88c2ced9b675f1702a137b7
Alexis Burgos (gtm) <34293632+AlexisBurgosGtm@users.noreply.github.com>
2021-12-02 15:04:49 -0600

    Update viewRecetas.js

--- build/views/viewRecetas.js
+++ build/views/viewRecetas.js
@@ -53,6 +53,8 @@ function getView(){
                     </div>
                     <div class="modal-body">
                        
+                        
+
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
